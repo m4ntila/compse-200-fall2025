e0232b171b9e23fe8054e370fa04c1ba3259d9bc
Antti Peltola <antti.peltola@tuni.fi>
2025-11-26 20:08:31 +0200

    Add jest.config.js file

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: "node",
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: ["lcov", "text", "clover"],
+    coveragePathIgnorePatterns: [
+      "/node_modules/",
+      "/src/.internal/"
+    ]
+  };
